1b2cb922ce34e46498d8d0d5b2aeb7d37012f454
lappanchappan43 <sunil43thapa@gmail.com>
2021-12-07 15:05:12 +0545

    Hair colorization

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ from dnn_face_detection import detect_face
 
 model = load_model('model.h5')
 
+st.title("Hair Color Segmentation")
+
 def predict(image):
     return model.predict(np.asarray([image]) ).reshape((224,224))
 
@@ -20,27 +22,17 @@ def display(img):
     plt.show()
 
 data = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
-print(data)
+
 if data is not None:
-    st.image(data, use_column_width=True)
     img = np.asarray(bytearray(data.read()), dtype=np.uint8)
     img = cv2.imdecode(img, 1)
-    # cv2.imshow('image',img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
     
     bounding_box = detect_face(img)
-    print(bounding_box)
 
     for box in bounding_box:
         (x,y,w,h) = box.astype("int")
-        x -= 200
-        y -= 200
-        w += 100
 
         crop_face = img[0:h, 0:img.shape[1]]
-        
-        print(x,y,w,h)
 
     res_img = cv2.resize(crop_face, (224, 224))
     img = cv2.cvtColor(crop_face, cv2.COLOR_BGR2GRAY)
@@ -48,15 +40,58 @@ if data is not None:
     pred_image = resize(img,(224,224)).reshape((224,224,1))
     pred = predict(pred_image)
     rgb_img = cv2.cvtColor(pred, cv2.COLOR_GRAY2RGB)
-    # rgb_img[pred2>0.5] = (255, 0, 0)
-    # rgb_img[pred2<=0.5] = (0, 0, 0)
-    rgb_img[pred>0.5] = (105, 105, 105)
+
+    title_container = st.container()
+    col1, col2 = st.columns([10, 1])
+
+    with title_container:
+        with col1:
+            slide = st.slider("Color Fade Value", 0.0, 1.0, step=0.1)
+        with col2:
+            color = st.color_picker('Pick A Color', '#00f900')
+    hex_strip = color.lstrip('#')
+    rgb = tuple(int(hex_strip[i:i+2], 16) for i in (0, 2, 4))
+    
+    # rgb_img[pred>0.5] = (105, 105, 105)
+    rgb_img[pred>0.5] = rgb
     norm_image = cv2.normalize(rgb_img, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
     norm_image = norm_image.astype(np.uint8)
-    # combined = cv2.bitwise_and(res_img, res_img, mask=norm_image)
+    
     new_img = cv2.cvtColor(res_img, cv2.COLOR_BGR2RGB)
-    # combined = cv2.add(norm_image, new_img)
-    # combined = cv2.cvtColor(combined, cv2.COLOR_BGR2RGB)
-    combined = cv2.addWeighted(new_img, 1, norm_image, 0.3, 0)
-    # st.write(combined)
-    st.image(combined, use_column_width=True)
+    
+    combined = cv2.addWeighted(new_img, 1, norm_image, slide, 0)
+    
+    title_container = st.container()
+    col1, col2 = st.columns(2)
+    with title_container:
+        with col1:
+            col1.header("Original")
+            col1.image(new_img, use_column_width=True)
+        with col2:
+            col2.header("Colored")
+            col2.image(combined, use_column_width=True)
+
+st.markdown("""
+    <hr>
+""", unsafe_allow_html=True)
+
+def resize_img(img_path):
+    img = cv2.imread(img_path)
+    res_img = cv2.resize(img, (224, 224))
+    new_img = cv2.cvtColor(res_img, cv2.COLOR_BGR2RGB)
+    return new_img
+
+st.subheader("Samples")
+title_container = st.container()
+col1, col2 = st.columns(2)
+with title_container:
+    with col1:
+        col1.image(resize_img('sample_results/1.jpg'), use_column_width=True)
+        col1.image(resize_img('sample_results/2.jpg'), use_column_width=True)
+        col1.image(resize_img('sample_results/3.jpg'), use_column_width=True)
+        col1.image(resize_img('sample_results/5.jpg'), use_column_width=True)
+    with col2:
+        col2.image(resize_img('sample_results/1_result.jpeg'), use_column_width=True)
+        col2.image(resize_img('sample_results/2_result.jpeg'), use_column_width=True)
+        col2.image(resize_img('sample_results/3_result.jpeg'), use_column_width=True)
+        col2.image(resize_img('sample_results/5_result.jpeg'), use_column_width=True)
